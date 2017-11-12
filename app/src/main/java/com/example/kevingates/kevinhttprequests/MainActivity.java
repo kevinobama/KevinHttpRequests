27d332a5cfcc4ea3874978e697196f5ba627c449
kevin gates <kevinobamatheus@gmail.com>
2017-11-12 11:40:14 +0800

    list view

--- app/src/main/java/com/example/kevingates/kevinhttprequests/MainActivity.java
+++ app/src/main/java/com/example/kevingates/kevinhttprequests/MainActivity.java
@@ -14,12 +14,34 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        String[] foods = {"Bacon", "Ham", "Tuna", "Candy", "Meatball", "Potato"};
+        ListAdapter buckysAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, foods);
+        ListView buckysListView = (ListView) findViewById(R.id.ListViewOne);
+        buckysListView.setAdapter(buckysAdapter);
+
+//        buckysListView.setOnItemClickListener(
+//                new AdapterView.OnItemClickListener(){
+//                    @Override
+//                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                        String food = String.valueOf(parent.getItemAtPosition(position));
+//                        Toast.makeText(MainActivity.this, food, Toast.LENGTH_LONG).show();
+//                    }
+//                }
+//        );
     }
 
     public void sendGetRequest(View View) {
